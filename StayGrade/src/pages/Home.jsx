f28bdc3a68641e2a00994f68d88668da924b9844
KeaneEdbertCandra <keane.candra16@gmail.com>
2026-05-22 18:12:27 +0700

    bikin 2 komponen dan nyoba HotelCard di Home.jsx

--- StayGrade/src/pages/Home.jsx
+++ StayGrade/src/pages/Home.jsx
@@ -1,6 +1,13 @@
 import { A } from "@solidjs/router"
+import { For } from "solid-js"
+import HotelCard from "../components/HotelCard";
 
 function Home() {
+  const hotels = [
+    { id: 1, image: "aaa", name: "Hotel A", location: "Menteng, Jakarta", prices: "100-200", rating: "4.8", reviewCount: "70" },
+    { id: 2, image: "aaa", name: "Hotel B", location: "Taman Kopo indah, Bandung", prices: "100-200", rating: "4.3", reviewCount: "10" },
+]
+  
   return (
     <div>
       <h1>Home Page</h1>
@@ -10,7 +17,18 @@ function Home() {
       <br></br>
       <A href="/register">Register Page </A>
 
-      
+      <For each={hotels}>
+          {(hotel) => (
+            <HotelCard
+              image={hotel.image}
+              name={hotel.name}
+              location={hotel.location}
+              prices={hotel.prices}
+              rating={hotel.rating}
+              reviewCount={hotel.reviewCount}
+            />
+          )}
+      </For>
     </div>
   );
 };
