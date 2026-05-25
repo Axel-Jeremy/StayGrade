add66664e327818e2731b5f04e1ae95b7c89547c
Axel-Jeremy <axeljeremy2802@gmail.com>
2026-05-25 14:05:09 +0700

    fix register style

--- StayGrade/src/pages/Register.jsx
+++ StayGrade/src/pages/Register.jsx
@@ -10,31 +10,35 @@ function Register() {
         navigate('/')
     }
     return (
-        <div class={style.containerRoot}>
-            <div class={style.mainContainer}>
-                <h1>Register</h1>
-                <nav>
-                    <A href="/">Home Page</A>
-                </nav>
-                <div>
-                    <h2 class={style.textLabel}>Nama Lengkap*</h2>
-                    <input type="text" placeholder="Masukkan Nama Lengkap Anda" class={style.inputField} />
-                    <h2 class={style.textLabel}>Email*</h2>
-                    <input type="text" placeholder="Masukkan Email Anda" class={style.inputField} />
-                    <h2 class={style.textLabel}>Password*</h2>
-                    <input type="text" placeholder="Masukkan Password Anda" class={style.inputField} />
-                    <h2 class={style.textLabel}>Konfirmasi Password*</h2>
-                    <input type="text" placeholder="Masukkan Konfirmasi Password Anda" class={style.inputField} />
-                </div>
 
+        <div class={style.mainContainer}>
+            <h1>Register</h1>
+            {/* <nav>
+                    <A href="/">Home Page</A>
+                </nav> */}
+            <div>
+                <h3 class={style.textLabel}>Nama Lengkap*</h3>
+                <input type="text" placeholder="Masukkan Nama Lengkap Anda" class={style.inputField} />
+                <h3 class={style.textLabel}>Email*</h3>
+                <input type="text" placeholder="Masukkan Email Anda" class={style.inputField} />
+                <h3 class={style.textLabel}>Password*</h3>
+                <input type="text" placeholder="Masukkan Password Anda" class={style.inputField} />
+                <h3 class={style.textLabel}>Konfirmasi Password*</h3>
+                <input type="text" placeholder="Masukkan Konfirmasi Password Anda" class={style.inputField} />
+            </div>
+
+            <div class={style.buttonContainer}>
                 <button onClick={handleClick} class={style.btnsubmit}>
                     Register
                 </button>
-                <div>
-                    <label>Sudah Punya Akun?</label><A href="/login">Log In</A>
+                <div class={style.link}>
+                    <label>Sudah Punya Akun?</label>
+                    <A href="/login">Log In</A>
                 </div>
             </div>
+
         </div>
+
     );
 };
 
