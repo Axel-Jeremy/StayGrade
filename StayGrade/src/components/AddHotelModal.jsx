405ecc72df6a09acc2fa8e4b96e25395d9226632
KeaneEdbertCandra <keane.candra16@gmail.com>
2026-06-01 18:44:49 +0700

    add hotel beres

--- StayGrade/src/components/AddHotelModal.jsx
+++ StayGrade/src/components/AddHotelModal.jsx
@@ -1,6 +1,53 @@
 import "../style/AddHotelModal.css";
+import { createSignal } from "solid-js";
 
 function AddHotelModal(props) {
+
+    const [name, setName] = createSignal("");
+    const [location, setLocation] = createSignal("");
+    const [image, setImage] = createSignal("");
+    const [description, setDescription] = createSignal("");
+    const [facilities, setFacilities] = createSignal("");
+    const [price, setPrice] = createSignal("");
+
+    async function handleAddHotel(e) {
+        e.preventDefault(); // Mencegah halaman me-refresh otomatis saat form di-submit
+
+        // Validasi agar tidak ada yang kosong
+        if (!name() || !location() || !image() || !description() || !facilities() || !price()) {
+            alert("Semua field bertanda * wajib diisi!");
+            return;
+        }
+
+        try {
+            const response = await fetch(`http://localhost:5000/api/hotels`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    name: name(),
+                    location: location(),
+                    image: image(),
+                    description: description(),
+                    facilities: facilities(),
+                    price: price(),
+                })
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                alert("Hotel berhasil ditambahkan!");
+                props.onClose(); 
+                window.location.reload;
+            } else {
+                alert("Gagal menambahkan hotel: " + data.message);
+            }
+        } catch (error) {
+            console.error("Error adding hotel:", error);
+            alert("Terjadi kesalahan pada server");
+        }
+    }
+    
     return (
         <div class="modal-overlay">
             <div class="hotel-modal">
@@ -23,6 +70,8 @@ function AddHotelModal(props) {
                         <input
                             type="text"
                             placeholder="Masukkan Nama Hotel"
+                            onInput={(e) => setName(e.target.value)}
+                            value={name()}
                         />
                     </div>
 
@@ -31,6 +80,8 @@ function AddHotelModal(props) {
                         <input
                             type="text"
                             placeholder="Contoh: Jakarta, Indonesia"
+                            onInput={(e) => setLocation(e.target.value)}
+                            value={location()}
                         />
                     </div>
 
@@ -39,6 +90,8 @@ function AddHotelModal(props) {
                         <textarea
                             rows="3"
                             placeholder="Preview Gambar"
+                            onInput={(e) => setImage(e.target.value)}
+                            value={image()}
                         />
                     </div>
 
@@ -47,6 +100,8 @@ function AddHotelModal(props) {
                         <textarea
                             rows="4"
                             placeholder="Deskripsi Hotel"
+                            onInput={(e) => setDescription(e.target.value)}
+                            value={description()}
                         />
                     </div>
 
@@ -55,6 +110,18 @@ function AddHotelModal(props) {
                         <input
                             type="text"
                             placeholder="Contoh: Wifi, Breakfast, Dll"
+                            onInput={(e) => setFacilities(e.target.value)}
+                            value={facilities()}
+                        />
+                    </div>
+
+                    <div class="form-group">
+                        <label>Harga *</label>
+                        <input
+                            type="text"
+                            placeholder="Contoh: 50 - 100"
+                            onInput={(e) => setPrice(e.target.value)}
+                            value={price()}
                         />
                     </div>
 
@@ -70,6 +137,7 @@ function AddHotelModal(props) {
                         <button
                             type="submit"
                             class="post-button"
+                            onclick={handleAddHotel}
                         >
                             Post
                         </button>
