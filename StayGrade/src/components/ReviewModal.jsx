58de2e9aff0c68f0f142d4671c46afb72b79180a
KeaneEdbertCandra <keane.candra16@gmail.com>
2026-06-09 20:14:08 +0700

    kelupaan inclue

--- StayGrade/src/components/ReviewModal.jsx
+++ StayGrade/src/components/ReviewModal.jsx
@@ -30,6 +30,7 @@ function ReviewModal(props) {
     try {
       const response = await fetch("http://localhost:5000/api/reviews", {
         method: "POST",
+        credentials: "include",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(templateData),
       });
